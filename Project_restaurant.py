c1d9294cb5ad900b5e3e44e819426990372bcc59
Juanadelacuesta <juanita.dlc@gmail.com>
2016-01-26 10:49:33 -0500

    Type: Styling Add .css file with style

--- Project_restaurant.py
+++ Project_restaurant.py
@@ -2,7 +2,7 @@ import cgi
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem, engine
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 app = Flask(__name__)
 
 Base.metadata.bind = engine
@@ -51,11 +51,13 @@ def AddMenuItem():
         
         session.add(new_item)
         session.commit()
+        flash('New Item created!')
         return redirect(url_for('index'))
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menuitem_id>/', 
             methods = ['GET','POST'])    
 def editMenuItem(restaurant_id, menuitem_id):
+    print "edit"
     restaurant = (session.query(Restaurant).
         filter(Restaurant.id==restaurant_id).one())
     item = (session.query(MenuItem).
@@ -69,24 +71,29 @@ def editMenuItem(restaurant_id, menuitem_id):
             item.name = request.form['name']
         if request.form['course'] != item.course:
             item.course = request.form['course']
-        if request.form['description'] != item.description:
-            item.description = request.form['description']
+        if request.form['desc'] != item.description:
+            item.description = request.form['desc']
         if request.form['price'] != item.price:
-            print "price"
             item.price = request.form['price']
-        if request.form['restaurant_id'] != item.restaurant_id:
-            item.restaurant_id = request.form['restaurant_id']   
+        item.restaurant_id = restaurant_id  
         session.commit()
-        return  "blah"
-        #redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
-   
-    
-@app.route('/restaurants/delete/<int:menuitem_id>/')    
+        return  redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
+      
+@app.route('/restaurants/delete/<int:menuitem_id>/', methods = ['GET','POST'])    
 def deleteMenuItem(menuitem_id):
     item = (session.query(MenuItem).
-        filter(MenuItem.id==menuitem_id).one())
-    return render_template('deleteMenuItem.html', item=item)
-    
+                filter(MenuItem.id==menuitem_id).one())
+    if request.method == 'GET':
+        return render_template('deleteMenuItem.html', item=item)
+        
+    elif request.method == 'POST':
+        flash("Item deleted!")
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('index'))
+        
 if __name__ == '__main__':
+    app.secret_key = 'Secret_key    '
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
+    
